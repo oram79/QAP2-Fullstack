81a0183b83eef629ab5e80669d85b5b5c58d5790
Logan Oram <logan.oram@keyin.com>
2024-10-16 13:49:02 -0230

    index.js updated (with comments)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // In-memory data storage
-
 let streaks = [];
 let currentStreak = 0;
 let currentQuestion = {};
@@ -13,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
+// Rendering main page //
 app.get('/', (req, res) => {
     res.render('index', {
       currentStreak,
@@ -21,17 +20,18 @@ app.get('/', (req, res) => {
     });
   });
   
-
+// Rendering The Quiz Page //
 app.get('/quiz', (req, res) => {
     currentQuestion = getQuestion();
     res.render('quiz', { currentQuestion, currentStreak });
   });
 
-
+// Rendering the gme over screen //
   app.get('/quiz-complete', (req, res) => {
     res.render('completion', { currentStreak });
   });
 
+// Rendering the leaderboard page //
   app.get('/leaderboard', (req, res) => {
     const topStreaks = streaks
       .sort((a, b) => b.correctAnswers - a.correctAnswers)
@@ -39,7 +39,7 @@ app.get('/quiz', (req, res) => {
   
     res.render('leaderboards', { topStreaks });
   });
-
+// Sending data for correct anwsers then displaying
   app.post('/quiz', (req, res) => {
     const userAnswer = parseFloat(req.body.answer);
     const isCorrect = userAnswer === currentQuestion.answer;
@@ -53,7 +53,7 @@ app.get('/quiz', (req, res) => {
   
     res.redirect(isCorrect ? '/quiz' : '/quiz-complete');
   });
-  
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
