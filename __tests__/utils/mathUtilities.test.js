0c2354dabd73e44cf9450a7a3b3caea8753ae14d
Logan Oram <logan.oram@keyin.com>
2024-10-16 18:49:55 -0230

    tests

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,9 +1,21 @@
 const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
-describe("Tests for getQuestion", () => {
+describe('getQuestion', () => {
+    it('should generate a question with a valid answer', () => {
+      const question = generateQuestion();
+      expect(typeof question.question).toBe('string');
+      expect(typeof question.answer).toBe('number');
+    });
+  });
 
-});
-
-describe("Tests for isCorrectAnswer", () => {
-    
-});
+  describe('isCorrectAnswer', () => {
+    it('should return true for a correct answer', () => {
+      const question = { question: '2 + 2', answer: 4 };
+      expect(checkAnswer(question, 4)).toBe(true);
+    });
+  
+    it('should return false for an incorrect answer', () => {
+      const question = { question: '2 + 2', answer: 4 };
+      expect(checkAnswer(question, 5)).toBe(false);
+    });
+  });
